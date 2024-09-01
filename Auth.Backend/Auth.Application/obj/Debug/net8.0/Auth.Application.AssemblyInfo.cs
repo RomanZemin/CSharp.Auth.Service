1d0403403068608af029a0966f60cfecb1b8a1d4
RomanZemin <RomanZemin12@yandex.ru>
2024-09-01 22:24:25 +0700

    cache

--- Auth.Backend/Auth.Application/obj/Debug/net8.0/Auth.Application.AssemblyInfo.cs
+++ Auth.Backend/Auth.Application/obj/Debug/net8.0/Auth.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auth.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+933ea0e5814820baddab87473428d165083fec5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+681971123d258d14f30947c4b87bda7c18c4d41e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auth.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auth.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
