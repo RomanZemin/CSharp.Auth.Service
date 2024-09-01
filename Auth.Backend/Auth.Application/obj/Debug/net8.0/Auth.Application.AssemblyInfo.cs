7a6075733d48b2950240e5e5551b9683826f413c
RomanZemin <RomanZemin12@yandex.ru>
2024-09-01 22:30:54 +0700

    cache

--- Auth.Backend/Auth.Application/obj/Debug/net8.0/Auth.Application.AssemblyInfo.cs
+++ Auth.Backend/Auth.Application/obj/Debug/net8.0/Auth.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auth.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc7e1b9cdf976d80901e67f7873da75edab82cff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bb7cc61f97771aaad5e8b60603aad58980ab3e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auth.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auth.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Auth.Backend/Auth.Domain/obj/Debug/net8.0/Auth.Domain.AssemblyInfo.cs
+++ Auth.Backend/Auth.Domain/obj/Debug/net8.0/Auth.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auth.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14bdd9a3b8b44383ff1340a5b36e05f6948c1f96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bb7cc61f97771aaad5e8b60603aad58980ab3e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auth.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auth.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
